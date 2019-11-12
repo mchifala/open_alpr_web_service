b2d69b63c4557e12deb71209d7a6222ff09ba5cf
mchifala <mich5456@colorado.edu>
2019-11-12 15:59:08 -0700

    Add modified launch scripts and client launch script

--- rabbitmq/rabbitmq-launch.sh
+++ rabbitmq/rabbitmq-launch.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-#
-# This is the script you need to provide to launch a rabbitmq instance
-# service
 
 #gcloud container clusters get-credentials lab8
 
 docker pull rabbitmq
-kubectl create deployment lab8-rabbitmq --image=rabbitmq
-kubectl expose deployment lab8-rabbitmq--port 5672
+kubectl create deployment rabbitmq --image=rabbitmq
+kubectl expose deployment rabbitmq --port 5672

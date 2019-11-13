47ede2b2f13e93fa5cd01e03baa73655050a2ff4
mchifala <mich5456@colorado.edu>
2019-11-13 17:43:14 +0000

    Add worker Dockerfile and worker-launch script

--- worker/worker-launch.sh
+++ worker/worker-launch.sh
@@ -2,4 +2,8 @@
 #
 # This is the script you need to provide to launch a redis instance
 # and cause it to run the redis-install.sh script
-#
+docker build -t worker:0.1 .
+docker tag worker:0.1 gcr.io/csci5253/worker:0.1
+docker push gcr.io/csci5253/worker:0.1
+kubectl create deployment worker --image=gcr.io/csci5253/worker:0.1
+kubectl scale deployment worker --replicas=3

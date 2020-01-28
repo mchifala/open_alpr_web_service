51dd4123c68b15989c3d339fa69b4943b4634737
mchifala <mich5456@colorado.edu>
2020-01-28 15:24:41 -0700

    Modify rabbmitmq, redis, and vm setup scripts

--- rabbitmq/rabbitmq-launch.sh
+++ rabbitmq/rabbitmq-launch.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-#gcloud container clusters get-credentials lab8
+# The following command must be run to get the Google Kubernetes Engine
+# credentials for a particular cluster. It must be run prior to executing any of
+# the launch shell scripts. 
+# gcloud container clusters get-credentials [cluster_name]
+
+# This script pulls the latest RabbitMQ image from the Docker Hub and deploys
+# the container in a pod with the name 'rabbitmq' in the Kubernetes cluster.
+# It also exposes port 5672 for the redis service (the default RabbitMQ port)
 
 docker pull rabbitmq
 kubectl create deployment rabbitmq --image=rabbitmq
